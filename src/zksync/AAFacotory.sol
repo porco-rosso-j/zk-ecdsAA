817ec21e6a8ee511d0da4e199a412fa685c691e0
Porco <porcorossoj89@gmail.com>
2023-06-09 10:52:09 +0200

    factory

---
+++ src/zksync/AAFacotory.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "era-system-contracts/contracts/Constants.sol";
+import "era-system-contracts/contracts/libraries/SystemContractsCaller.sol";
+
+contract AAFactory {
+    bytes32 public aaBytecodeHash;
+
+    constructor(bytes32 _aaBytecodeHash) {
+        aaBytecodeHash = _aaBytecodeHash;
+    }
+
+    function deployAccount(
+        bytes32 salt,
+        address owner1,
+        address owner2
+    ) external returns (address accountAddress) {
+        (bool success, bytes memory returnData) = SystemContractsCaller
+            .systemCallWithReturndata(
+                uint32(gasleft()),
+                address(DEPLOYER_SYSTEM_CONTRACT),
+                uint128(0),
+                abi.encodeCall(
+                    DEPLOYER_SYSTEM_CONTRACT.create2Account,
+                    (
+                        salt,
+                        aaBytecodeHash,
+                        abi.encode(owner1, owner2),
+                        IContractDeployer.AccountAbstractionVersion.Version1
+                    )
+                )
+            );
+        require(success, "Deployment failed");
+
+        (accountAddress) = abi.decode(returnData, (address));
+    }
+}
