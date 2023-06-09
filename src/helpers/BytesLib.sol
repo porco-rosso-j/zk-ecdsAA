92d9f4be18405493ed4390781d5fcd912d27a912
Porco <porcorossoj89@gmail.com>
2023-06-09 10:49:04 +0200

    zksync

---
+++ src/helpers/BytesLib.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.8.12;
+
+library BytesLib {
+    function decodeRecoveryArgs(
+        bytes memory _calldata
+    ) internal pure returns (bytes32) {
+        bytes memory data = extractCalldata(_calldata);
+        (bytes32 newOwner, bytes32 guradian) = abi.decode(
+            data,
+            (bytes32, bytes32)
+        );
+        return guradian;
+    }
+
+    function decodeProposeInheritanceArgs(
+        bytes memory _calldata
+    ) internal pure returns (bytes32) {
+        bytes memory data = extractCalldata(_calldata);
+        bytes32 beneficiary = abi.decode(data, (bytes32));
+        return beneficiary;
+    }
+
+    function decodeExecuteInheritanceArgs(
+        bytes memory _calldata
+    ) internal pure returns (bytes32) {
+        bytes memory data = extractCalldata(_calldata);
+        (bytes32 beneficiary, uint inheritanceCount) = abi.decode(
+            data,
+            (bytes32, uint)
+        );
+        return beneficiary;
+    }
+
+    function extractCalldata(
+        bytes memory _calldata
+    ) internal pure returns (bytes memory) {
+        bytes memory data;
+
+        require(_calldata.length >= 4);
+
+        assembly {
+            let totalLength := mload(_calldata)
+            let targetLength := sub(totalLength, 4)
+            data := mload(0x40)
+
+            mstore(data, targetLength)
+            mstore(0x40, add(0x20, targetLength))
+            mstore(add(data, 0x20), shl(0x20, mload(add(_calldata, 0x20))))
+
+            for {
+                let i := 0x1C
+            } lt(i, targetLength) {
+                i := add(i, 0x20)
+            } {
+                mstore(
+                    add(add(data, 0x20), i),
+                    mload(add(add(_calldata, 0x20), add(i, 0x04)))
+                )
+            }
+        }
+
+        return data;
+    }
+
+    function getSelector(bytes memory _data) internal pure returns (bytes4) {
+        bytes4 selector;
+        assembly {
+            selector := calldataload(_data)
+        }
+        return selector;
+    }
+}
