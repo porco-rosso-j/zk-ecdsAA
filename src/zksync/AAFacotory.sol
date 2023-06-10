8e769ad9f3a1318225babe179093616e47333de3
Porco <porcorossoj89@gmail.com>
2023-06-10 15:44:10 +0200

    zksync imp

--- src/zksync/AAFacotory.sol
+++ src/zksync/AAFacotory.sol
@@ -1,37 +1,37 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.16;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
 
-// import "era-system-contracts/contracts/Constants.sol";
-// import "era-system-contracts/contracts/libraries/SystemContractsCaller.sol";
+import "era-system-contracts/contracts/Constants.sol";
+import "era-system-contracts/contracts/libraries/SystemContractsCaller.sol";
 
-// contract AAFactory {
-//     bytes32 public aaBytecodeHash;
+contract AAFactory {
+    bytes32 public aaBytecodeHash;
 
-//     constructor(bytes32 _aaBytecodeHash) {
-//         aaBytecodeHash = _aaBytecodeHash;
-//     }
+    constructor(bytes32 _aaBytecodeHash) {
+        aaBytecodeHash = _aaBytecodeHash;
+    }
 
-//     function deployAccount(
-//         bytes32 salt,
-//         bytes32 owner,
-//     ) external returns (address accountAddress) {
-//         (bool success, bytes memory returnData) = SystemContractsCaller
-//             .systemCallWithReturndata(
-//                 uint32(gasleft()),
-//                 address(DEPLOYER_SYSTEM_CONTRACT),
-//                 uint128(0),
-//                 abi.encodeCall(
-//                     DEPLOYER_SYSTEM_CONTRACT.create2Account,
-//                     (
-//                         salt,
-//                         aaBytecodeHash,
-//                         abi.encode(owner),
-//                         IContractDeployer.AccountAbstractionVersion.Version1
-//                     )
-//                 )
-//             );
-//         require(success, "Deployment failed");
+    function deployAccount(
+        bytes32 salt,
+        bytes32 owner,
+    ) external returns (address accountAddress) {
+        (bool success, bytes memory returnData) = SystemContractsCaller
+            .systemCallWithReturndata(
+                uint32(gasleft()),
+                address(DEPLOYER_SYSTEM_CONTRACT),
+                uint128(0),
+                abi.encodeCall(
+                    DEPLOYER_SYSTEM_CONTRACT.create2Account,
+                    (
+                        salt,
+                        aaBytecodeHash,
+                        abi.encode(owner),
+                        IContractDeployer.AccountAbstractionVersion.Version1
+                    )
+                )
+            );
+        require(success, "Deployment failed");
 
-//         (accountAddress) = abi.decode(returnData, (bytes32));
-//     }
-// }
+        (accountAddress) = abi.decode(returnData, (bytes32));
+    }
+}
--- src/zksync/Account.sol
+++ src/zksync/Account.sol
@@ -1,209 +1,249 @@
-// // https://github.com/matter-labs/foundry-zksync
-// // https://github.com/sammyshakes/sample-fzksync-project/blob/main/src/TwoUserMultiSig.sol
+// https://github.com/matter-labs/foundry-zksync
+// https://github.com/sammyshakes/sample-fzksync-project/blob/main/src/TwoUserMultiSig.sol
 
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.12;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
 
-// import "era-system-contracts/contracts/interfaces/IAccount.sol";
-// import "era-system-contracts/contracts/libraries/TransactionHelper.sol";
-// import "openzeppelin-contracts/contracts/interfaces/IERC1271.sol";
+import "era-system-contracts/contracts/interfaces/IAccount.sol";
+import "era-system-contracts/contracts/libraries/TransactionHelper.sol";
+import "openzeppelin-contracts/contracts/interfaces/IERC1271.sol";
 
-// // Access zkSync system contracts for nonce validation via NONCE_HOLDER_SYSTEM_CONTRACT
-// import "era-system-contracts/contracts/Constants.sol";
-// // to call non-view function of system contracts
-// import "era-system-contracts/contracts/libraries/SystemContractsCaller.sol";
+// Access zkSync system contracts for nonce validation via NONCE_HOLDER_SYSTEM_CONTRACT
+import "era-system-contracts/contracts/Constants.sol";
+// to call non-view function of system contracts
+import "era-system-contracts/contracts/libraries/SystemContractsCaller.sol";
 
-// import "src/helpers/BytesLib.sol";
+import "../modules/Inheritance.sol";
+import "../modules/Recovery.sol";
+import "../helpers/BytesLib.sol";
+import "../erc4337/UltraVerifier.sol";
 
-// contract Account is IAccount, IERC1271 {
-//     // to get transaction hash
-//     using TransactionHelper for Transaction;
-//     using BytesLib for bytes;
+contract Account is
+    IAccount,
+    IERC1271,
+    UltraVerifier,
+    RecoveryModule,
+    InheritanceModule
+{
+    // to get transaction hash
+    using TransactionHelper for Transaction;
+    using BytesLib for bytes;
 
-//     // state variables for account owners
-//     bytes32 public owner;
+    UltraVerifier public verifier;
 
-//     bytes4 constant EIP1271_SUCCESS_RETURN_VALUE = 0x1626ba7e;
+    // state variables for account owners
+    bytes32 public owner;
 
-//     modifier onlyBootloader() {
-//         require(
-//             msg.sender == BOOTLOADER_FORMAL_ADDRESS,
-//             "Only bootloader can call this function"
-//         );
-//         // Continue execution if called from the bootloader.
-//         _;
-//     }
+    bytes4 constant EIP1271_SUCCESS_RETURN_VALUE = 0x1626ba7e;
 
-//     constructor(bytes32 _owner) {
-//         owner = _owner;
-//     }
+    modifier onlyBootloader() {
+        require(
+            msg.sender == BOOTLOADER_FORMAL_ADDRESS,
+            "Only bootloader can call this function"
+        );
+        // Continue execution if called from the bootloader.
+        _;
+    }
 
-//     function validateTransaction(
-//         bytes32,
-//         bytes32 _suggestedSignedHash,
-//         Transaction calldata _transaction
-//     ) external payable override onlyBootloader returns (bytes4 magic) {
-//         return _validateTransaction(_suggestedSignedHash, _transaction);
-//     }
+    constructor(
+        bytes32 _owner,
+        address _verifier,
+        bytes32[] memory _guardians,
+        uint8 _threshold,
+        uint _pendingPeriod,
+        bytes32 _beneficiary
+    ) {
+        owner = _owner;
+        verifier = UltraVerifier(_verifier);
+        initializeRecovery(_guardians, _threshold);
+        initializeInheritance(_pendingPeriod, _beneficiary);
+    }
 
-//     function _validateTransaction(
-//         bytes32 _suggestedSignedHash,
-//         Transaction calldata _transaction
-//     ) internal returns (bytes4 magic) {
-//         // Incrementing the nonce of the account.
-//         // Note, that reserved[0] by convention is currently equal to the nonce passed in the transaction
-//         SystemContractsCaller.systemCallWithPropagatedRevert(
-//             uint32(gasleft()),
-//             address(NONCE_HOLDER_SYSTEM_CONTRACT),
-//             0,
-//             abi.encodeCall(
-//                 INonceHolder.incrementMinNonceIfEquals,
-//                 (_transaction.nonce)
-//             )
-//         );
+    function validateTransaction(
+        bytes32,
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader returns (bytes4 magic) {
+        return _validateTransaction(_suggestedSignedHash, _transaction);
+    }
 
-//         bytes32 txHash;
-//         // While the suggested signed hash is usually provided, it is generally
-//         // not recommended to rely on it to be present, since in the future
-//         // there may be tx types with no suggested signed hash.
-//         if (_suggestedSignedHash == bytes32(0)) {
-//             txHash = _transaction.encodeHash();
-//         } else {
-//             txHash = _suggestedSignedHash;
-//         }
+    function _validateTransaction(
+        bytes32 _suggestedSignedHash,
+        Transaction calldata _transaction
+    ) internal returns (bytes4 magic) {
+        // Incrementing the nonce of the account.
+        // Note, that reserved[0] by convention is currently equal to the nonce passed in the transaction
+        SystemContractsCaller.systemCallWithPropagatedRevert(
+            uint32(gasleft()),
+            address(NONCE_HOLDER_SYSTEM_CONTRACT),
+            0,
+            abi.encodeCall(
+                INonceHolder.incrementMinNonceIfEquals,
+                (_transaction.nonce)
+            )
+        );
 
-//         // The fact there is are enough balance for the account
-//         // should be checked explicitly to prevent user paying for fee for a
-//         // transaction that wouldn't be included on Ethereum.
-//         uint256 totalRequiredBalance = _transaction.totalRequiredBalance();
-//         require(
-//             totalRequiredBalance <= address(this).balance,
-//             "Not enough balance for fee + value"
-//         );
+        bytes32 txHash;
+        // While the suggested signed hash is usually provided, it is generally
+        // not recommended to rely on it to be present, since in the future
+        // there may be tx types with no suggested signed hash.
+        if (_suggestedSignedHash == bytes32(0)) {
+            txHash = _transaction.encodeHash();
+        } else {
+            txHash = _suggestedSignedHash;
+        }
 
-//         if (
-//             isValidSignature(txHash, _transaction.signature) ==
-//             EIP1271_SUCCESS_RETURN_VALUE
-//         ) {
-//             magic = ACCOUNT_VALIDATION_SUCCESS_MAGIC;
-//         } else {
-//             magic = bytes4(0);
-//         }
-//     }
+        // The fact there is are enough balance for the account
+        // should be checked explicitly to prevent user paying for fee for a
+        // transaction that wouldn't be included on Ethereum.
+        uint256 totalRequiredBalance = _transaction.totalRequiredBalance();
+        require(
+            totalRequiredBalance <= address(this).balance,
+            "Not enough balance for fee + value"
+        );
 
-//     function executeTransaction(
-//         bytes32,
-//         bytes32,
-//         Transaction calldata _transaction
-//     ) external payable override onlyBootloader {
-//         _executeTransaction(_transaction);
-//     }
+        (bytes32 hashedAddr, bytes memory proof) = abi.decode(
+            _transaction.signature,
+            (bytes32, bytes)
+        );
 
-//     function _executeTransaction(Transaction calldata _transaction) internal {
-//         address to = address(uint160(_transaction.to));
-//         uint128 value = Utils.safeCastToU128(_transaction.value);
-//         bytes memory data = _transaction.data;
+        bytes32[] memory publicInputs = new bytes32[](33);
+        publicInputs[0] = hashedAddr;
 
-//         if (to == address(DEPLOYER_SYSTEM_CONTRACT)) {
-//             uint32 gas = Utils.safeCastToU32(gasleft());
+        bytes memory b = bytes.concat(txHash);
 
-//             // Note, that the deployer contract can only be called
-//             // with a "systemCall" flag.
-//             SystemContractsCaller.systemCallWithPropagatedRevert(
-//                 gas,
-//                 to,
-//                 value,
-//                 data
-//             );
-//         } else {
-//             bool success;
-//             assembly {
-//                 success := call(
-//                     gas(),
-//                     to,
-//                     value,
-//                     add(data, 0x20),
-//                     mload(data),
-//                     0,
-//                     0
-//                 )
-//             }
-//             require(success);
-//         }
-//     }
+        for (uint i = 0; i < b.length; i++) {
+            publicInputs[i + 1] = bytes32(uint(uint8(b[i])));
+        }
 
-//     function executeTransactionFromOutside(
-//         Transaction calldata _transaction
-//     ) external payable {
-//         _validateTransaction(bytes32(0), _transaction);
-//         _executeTransaction(_transaction);
-//     }
+        if (verifier.verify(proof, publicInputs)) {
+            magic = ACCOUNT_VALIDATION_SUCCESS_MAGIC;
+        } else {
+            magic = bytes4(0);
+        }
+    }
 
-//     function isValidSignature(
-//         bytes32 _hash,
-//         bytes memory _signature
-//     ) public view override returns (bytes4 magic) {
-//         magic = EIP1271_SUCCESS_RETURN_VALUE;
+    function executeTransaction(
+        bytes32,
+        bytes32,
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader {
+        _executeTransaction(_transaction);
+    }
 
-//         publicInputs = getPublicInputs(userOp, _hash, publicInputs);
+    function _executeTransaction(Transaction calldata _transaction) internal {
+        address to = address(uint160(_transaction.to));
+        uint128 value = Utils.safeCastToU128(_transaction.value);
+        bytes memory data = _transaction.data;
 
-//         // Note, that we should abstain from using the require here in order to allow for fee estimation to work
-//         if (!verifier.verify(userOp.signature, publicInputs)) {
-//             magic = bytes4(0);
-//         }
-//     }
+        if (to == address(DEPLOYER_SYSTEM_CONTRACT)) {
+            uint32 gas = Utils.safeCastToU32(gasleft());
 
-//     function getPublicInputs(
-//         UserOperation calldata userOp,
-//         bytes32 userOpHash,
-//         bytes32[] memory publicInputs
-//     ) internal view returns (bytes32[] memory) {
-//         bytes4 msgSig = BytesLib.getSelector(userOp.callData);
+            // Note, that the deployer contract can only be called
+            // with a "systemCall" flag.
+            SystemContractsCaller.systemCallWithPropagatedRevert(
+                gas,
+                to,
+                value,
+                data
+            );
+        } else {
+            bool success;
+            assembly {
+                success := call(
+                    gas(),
+                    to,
+                    value,
+                    add(data, 0x20),
+                    mload(data),
+                    0,
+                    0
+                )
+            }
+            require(success);
+        }
+    }
 
-//         // https://abi.hashex.org/
-//         publicInputs[0] = msgSig == bytes4(0x238bc149)
-//             ? BytesLib.decodeRecoveryArgs(userOp.callData)
-//             : msgSig == bytes4(0x11111111)
-//             ? BytesLib.decodeProposeInheritanceArgs(userOp.callData)
-//             : msgSig == bytes4(0x22222222)
-//             ? BytesLib.decodeExecuteInheritanceArgs(userOp.callData)
-//             : owner;
+    function executeTransactionFromOutside(
+        Transaction calldata _transaction
+    ) external payable {
+        _validateTransaction(bytes32(0), _transaction);
+        _executeTransaction(_transaction);
+    }
 
-//         bytes memory b = bytes.concat(userOpHash);
+    function isValidSignature(
+        bytes32 _hash,
+        bytes memory _signature
+    ) public view override returns (bytes4 magic) {
+        magic = EIP1271_SUCCESS_RETURN_VALUE;
 
-//         for (uint i = 0; i < b.length; i++) {
-//             publicInputs[i + 1] = bytes32(uint(uint8(b[i])));
-//         }
+        publicInputs = getPublicInputs(userOp, _hash, publicInputs);
 
-//         return publicInputs;
-//     }
+        // Note, that we should abstain from using the require here in order to allow for fee estimation to work
+        if (!verifier.verify(userOp.signature, publicInputs)) {
+            magic = bytes4(0);
+        }
+    }
 
-//     function payForTransaction(
-//         bytes32,
-//         bytes32,
-//         Transaction calldata _transaction
-//     ) external payable override onlyBootloader {
-//         bool success = _transaction.payToTheBootloader();
-//         require(success, "Failed to pay the fee to the operator");
-//     }
+    function getPublicInputs(
+        UserOperation calldata userOp,
+        bytes32 userOpHash,
+        bytes32[] memory publicInputs
+    ) internal view returns (bytes32[] memory) {
+        bytes4 msgSig = BytesLib.getSelector(userOp.callData);
 
-//     function prepareForPaymaster(
-//         bytes32, // _txHash
-//         bytes32, // _suggestedSignedHash
-//         Transaction calldata _transaction
-//     ) external payable override onlyBootloader {
-//         _transaction.processPaymasterInput();
-//     }
+        // https://abi.hashex.org/
+        publicInputs[0] = msgSig == bytes4(0x238bc149)
+            ? BytesLib.decodeRecoveryArgs(userOp.callData)
+            : msgSig == bytes4(0x11111111)
+            ? BytesLib.decodeProposeInheritanceArgs(userOp.callData)
+            : msgSig == bytes4(0x22222222)
+            ? BytesLib.decodeExecuteInheritanceArgs(userOp.callData)
+            : owner;
 
-//     fallback() external {
-//         // fallback of default account shouldn't be called by bootloader under no circumstances
-//         assert(msg.sender != BOOTLOADER_FORMAL_ADDRESS);
+        bytes memory b = bytes.concat(userOpHash);
 
-//         // If the contract is called directly, behave like an EOA
-//     }
+        for (uint i = 0; i < b.length; i++) {
+            publicInputs[i + 1] = bytes32(uint(uint8(b[i])));
+        }
 
-//     receive() external payable {
-//         // If the contract is called directly, behave like an EOA.
-//         // Note, that is okay if the bootloader sends funds with no calldata as it may be used for refunds/operator payments
-//     }
-// }
+        return publicInputs;
+    }
+
+    function payForTransaction(
+        bytes32,
+        bytes32,
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader {
+        bool success = _transaction.payToTheBootloader();
+        require(success, "Failed to pay the fee to the operator");
+    }
+
+    function prepareForPaymaster(
+        bytes32, // _txHash
+        bytes32, // _suggestedSignedHash
+        Transaction calldata _transaction
+    ) external payable override onlyBootloader {
+        _transaction.processPaymasterInput();
+    }
+
+    function changeOwner(bytes32 _owner) internal override {
+        owner = _owner;
+    }
+
+    function _owmer() internal view override returns (bytes32) {
+        return owner;
+    }
+
+    fallback() external {
+        // fallback of default account shouldn't be called by bootloader under no circumstances
+        assert(msg.sender != BOOTLOADER_FORMAL_ADDRESS);
+
+        // If the contract is called directly, behave like an EOA
+    }
+
+    receive() external payable {
+        // If the contract is called directly, behave like an EOA.
+        // Note, that is okay if the bootloader sends funds with no calldata as it may be used for refunds/operator payments
+    }
+}
